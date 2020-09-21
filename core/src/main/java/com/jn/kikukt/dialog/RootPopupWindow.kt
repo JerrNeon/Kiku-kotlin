2c651a57d7e2d1896ab1e8b1ce53f25ee3b4a1c5
stevie_chen <ch4416OO8979wei>
2020-09-21 17:38:27 +0800

    optimize

--- core/src/main/java/com/jn/kikukt/dialog/RootPopupWindow.kt
+++ core/src/main/java/com/jn/kikukt/dialog/RootPopupWindow.kt
@@ -20,47 +20,45 @@ abstract class RootPopupWindow {
     protected var mPopupWindow: PopupWindow? = null
     protected var mWindow: Window? = null
 
+    open val width: Int = 0//popupWindow width
+    open val height: Int = 0//popupWindow height
+    open val animationStyle: Int = 0//动画
+
     abstract val layoutResId: Int//LayoutResId
-    abstract val width: Int//popupWindow width
-    abstract val height: Int//popupWindow height
 
     constructor(activity: Activity) {
         this.mActivity = activity
         this.mContext = activity.applicationContext
         initView()
-        setWindowAttributes()
-        initData()
-        setListener()
     }
 
     constructor(fragment: Fragment) {
         this.mFragment = fragment
         this.mActivity = fragment.activity
-        this.mContext = fragment.requireActivity().applicationContext
+        this.mContext = fragment.context
         initView()
-        setWindowAttributes()
-        initData()
-        setListener()
     }
 
     protected fun initView() {
         mView = LayoutInflater.from(mContext).inflate(layoutResId, null, false)
         mPopupWindow = PopupWindow(mView).apply {
-            width = if (width == 0)
+            width = if (this@RootPopupWindow.width == 0)
                 mContext?.getScreenWidth() ?: ViewGroup.LayoutParams.WRAP_CONTENT
             else
-                width
-            height = if (height == 0)
+                this@RootPopupWindow.width
+            height = if (this@RootPopupWindow.height == 0)
                 ViewGroup.LayoutParams.WRAP_CONTENT
             else
-                height
-            isOutsideTouchable = false
+                this@RootPopupWindow.height
+            isFocusable = true//响应返回键(点击返回键消失)
+            isOutsideTouchable = true//点击边际是否可消失(true可消失)
+            if (this@RootPopupWindow.animationStyle != 0)
+                animationStyle = this@RootPopupWindow.animationStyle
         }
     }
 
     open fun setWindowAttributes() {
-        mWindow = mActivity!!.window
-        mWindow?.run {
+        mWindow = mActivity?.window?.apply {
             val lp = attributes
             lp.alpha = 0.5f
             addFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND)
@@ -68,9 +66,34 @@ abstract class RootPopupWindow {
         }
     }
 
-    open fun initData() {
+    open fun initData() {}
+
+    open fun setListener() {}
+
+    val isShowing
+        get() = mPopupWindow?.isShowing ?: false
+
+    fun dismiss() {
+        mPopupWindow?.dismiss()
     }
 
-    open fun setListener() {
+    fun setOnDismissListener(onDismissListener: PopupWindow.OnDismissListener) {
+        mPopupWindow?.setOnDismissListener(onDismissListener)
+    }
+
+    fun showAsDropDown(anchor: View) {
+        mPopupWindow?.showAsDropDown(anchor)
+    }
+
+    fun showAsDropDown(anchor: View, xoff: Int, yoff: Int) {
+        mPopupWindow?.showAsDropDown(anchor, xoff, yoff)
+    }
+
+    fun showAsDropDown(anchor: View, xoff: Int, yoff: Int, gravity: Int) {
+        mPopupWindow?.showAsDropDown(anchor, xoff, yoff, gravity)
+    }
+
+    fun showAtLocation(anchor: View, gravity: Int, x: Int, y: Int) {
+        mPopupWindow?.showAtLocation(anchor, gravity, x, y)
     }
 }
