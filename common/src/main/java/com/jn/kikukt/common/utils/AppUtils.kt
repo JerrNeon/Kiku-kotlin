692262c9ae7ab17a47a6589d6107f04f4064ba98
stevie_chen <ch4416OO8979wei>
2020-09-25 17:12:35 +0800

    optimize

--- common/src/main/java/com/jn/kikukt/common/utils/AppUtils.kt
+++ common/src/main/java/com/jn/kikukt/common/utils/AppUtils.kt
@@ -1,5 +1,6 @@
 package com.jn.kikukt.common.utils
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.app.ActivityManager
 import android.content.ComponentName
@@ -511,3 +512,43 @@ fun Context.getProperty(key: String): String {
     return ""
 }
 
+/**
+ * 获取手机唯一标识
+ */
+@SuppressLint("HardwareIds")
+fun Context.getAndroidID(): String? {
+    val androidID = Settings.Secure.getString(contentResolver, Settings.Secure.ANDROID_ID)
+    return EncodeUtils.str2Md5Hex2Lower(androidID + Build.SERIAL)
+}
+
+/**
+ * 获取手机设备唯一标识
+ */
+fun getDeviceId(): String? {
+    // If all else fails, if the user does have lower than API 9 (lower
+    // than Gingerbread), has reset their device or 'Secure.ANDROID_ID'
+    // returns 'null', then simply the ID returned will be solely based
+    // off their Android device information. This is where the collisions
+    // can happen.
+    // Thanks http://www.pocketmagic.net/?p=1662!
+    // Try not to use DISPLAY, HOST or ID - these items could change.
+    // If there are collisions, there will be overlapping data
+    val devIDShort =
+        "35" + Build.BOARD.length % 10 + Build.BRAND.length % 10 + Build.CPU_ABI.length % 10 + Build.DEVICE.length % 10 + Build.MANUFACTURER.length % 10 + Build.MODEL.length % 10 + Build.PRODUCT.length % 10
+    // http://stackoverflow.com/a/4789483/950427
+    // Only devices with API >= 9 have android.os.Build.SERIAL
+    // http://developer.android.com/reference/android/os/Build.html#SERIAL
+    // If a user upgrades software or roots their device, there will be a duplicate entry
+    var serial: String?
+    try {
+        serial = Build::class.java.getField("SERIAL")[null]?.toString() ?: "serial"
+        // Go ahead and return the serial for api => 9
+        return UUID(devIDShort.hashCode().toLong(), serial.hashCode().toLong()).toString()
+    } catch (exception: java.lang.Exception) {
+        // String needs to be initialized
+        serial = "serial" // some value
+    }
+    // http://stackoverflow.com/a/2853253/950427
+    // Finally, combine the values we have found by using the UUID class to create a unique identifier
+    return UUID(devIDShort.hashCode().toLong(), serial.hashCode().toLong()).toString()
+}
