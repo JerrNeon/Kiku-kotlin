2a1dbe2102e526098ec6b086d584d48076e3e41c
stevie_chen <ch4416OO8979wei>
2020-11-27 18:53:16 +0800

    optimize

--- core/src/main/java/com/jn/kikukt/utils/glide/GlideOnScrollListener.kt
+++ core/src/main/java/com/jn/kikukt/utils/glide/GlideOnScrollListener.kt
@@ -1,20 +1,32 @@
 package com.jn.kikukt.utils.glide
 
-import android.content.Context
+import android.app.Activity
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.RequestManager
 
 /**
  * Author：Stevie.Chen Time：2019/7/15
  * Class Comment：RecyclerView中Glide加载优化
  */
-class GlideOnScrollListener(val context: Context) : RecyclerView.OnScrollListener() {
+class GlideOnScrollListener : RecyclerView.OnScrollListener {
+
+    private var requestManager: RequestManager
+
+    constructor(activity: Activity) : super() {
+        requestManager = activity.requestManager()
+    }
+
+    constructor(fragment: Fragment) : super() {
+        requestManager = fragment.requestManager()
+    }
 
     override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
         super.onScrollStateChanged(recyclerView, newState)
         if (newState == RecyclerView.SCROLL_STATE_IDLE) {
-            GlideUtil.resumeRequests(context)
+            GlideUtil.resumeRequests(requestManager)
         } else {
-            GlideUtil.pauseRequests(context)
+            GlideUtil.pauseRequests(requestManager)
         }
     }
 }
